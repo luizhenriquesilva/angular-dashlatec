59287cce24195360ec431d11adf085b7e60e960f
luizhenriquesilva <luiz.h85@hotmail.com>
2019-11-03 22:26:05 -0300

    12 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css', '../css/sb-admin.min.css', '../css/sb-admin.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
