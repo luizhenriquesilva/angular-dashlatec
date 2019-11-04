e2edb9c5dccfbf675ee5259e5993037ca9d59e8c
luizhenriquesilva <luiz.h85@hotmail.com>
2019-11-04 00:23:24 -0300

    1 file modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css', '../css/sb-admin.min.css', '../css/sb-admin.css' ]
+  styleUrls: [ './app.component.css', '../css/sb-admin.min.css', '../css/sb-admin.css', '../css/darktable.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
