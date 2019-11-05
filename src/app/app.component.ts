d33593f5dbfada486a230d7bb48726fa9c23290d
luizhenriquesilva <luiz.h85@hotmail.com>
2019-11-05 08:38:09 -0300

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,81 +1,105 @@
-import { Component , ViewChild } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component, ViewChild } from "@angular/core";
+import { Chart } from "chart.js";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css', '../css/sb-admin.min.css', '../css/sb-admin.css', '../css/darktable.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "./app.component.css",
+    "../css/sb-admin.min.css",
+    "../css/sb-admin.css",
+    "../css/darktable.css"
+  ]
 })
-export class AppComponent  {
-  name = 'Angular   6';
+export class AppComponent {
+  name = "Angular   6";
   canvas: any;
   ctx: any;
-  @ViewChild('mychart') mychart;
+  @ViewChild("mychart") mychart;
 
   ngAfterViewInit() {
-    this.canvas = this.mychart.nativeElement; 
-    this.ctx = this.canvas.getContext('2d');
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext("2d");
 
     let myChart = new Chart(this.ctx, {
-      type: 'line',
-      
+      type: "line",
       data: {
-        datasets: [{
-          label: 'Höhenlinie',
-          backgroundColor: "rgba(255, 99, 132,0.4)",
-          borderColor: "rgb(255, 99, 132)",
-          fill: true,
-          data: [
-            { x: 1, y: 2 },
-            { x: 2500, y: 2.5 },
-            { x: 3000, y: 5 },
-            { x: 3400, y: 4.75 },
-            { x: 3600, y: 4.75 },
-            { x: 5200, y: 6 },
-            { x: 6000, y: 9 },
-            { x: 7100, y: 6 },
-          ],
-        }]
+        datasets: [
+          {
+            label: "Horas planejadas",
+            backgroundColor: "rgb(20, 255, 222)",
+            borderColor: "rgb(20, 255, 222)",
+            fill: false,
+            data: [
+              { x: 1, y: 2 },
+              { x: 1000, y: 2.5 },
+              { x: 1500, y: 5 },
+              { x: 2000, y: 4.75 },
+              { x: 3000, y: 4.75 },
+              { x: 5000, y: 6 },
+              { x: 7000, y: 7 },
+              { x: 8000, y: 6 }
+            ]
+          },
+          {
+            label: "Horas trabalhadas",
+            backgroundColor: "rgba(255, 99, 132,0.4)",
+            borderColor: "rgb(255, 99, 132)",
+            fill: false,
+            data: [
+              { x: 1, y: 2 },
+              { x: 1000, y: 2.5 },
+              { x: 1500, y: 4 },
+              { x: 2000, y: 5 },
+              { x: 3000, y: 3.75 },
+              { x: 5000, y: 6 },
+              { x: 7000, y: 5 },
+              { x: 8000, y: 6 }
+            ]
+          }
+        ]
       },
       options: {
         responsive: true,
         title: {
           display: true,
-          text: 'Höhenlinie'
+          text: "Horas planejadas x Horas trabalhadas"
         },
         scales: {
-          xAxes: [{
-            type: 'linear',
-            position: 'bottom',
-            ticks: {
-              userCallback: function (tick) {
-                if (tick >= 1000) {
-                  return (tick / 1000).toString() + 'km';
+          xAxes: [
+            {
+              type: "linear",
+              position: "bottom",
+              ticks: {
+                userCallback: function(tick) {
+                  if (tick >= 1000) {
+                    return (tick / 1000).toString() + "";
+                  }
+                  return tick.toString() + "m";
                 }
-                return tick.toString() + 'm';
+              },
+              scaleLabel: {
+                labelString: "Posto",
+                display: true
               }
-            },
-            scaleLabel: {
-              labelString: 'Länge',
-              display: true,
             }
-          }],
-          yAxes: [{
-            type: 'linear',
-            ticks: {
-              userCallback: function (tick) {
-                return tick.toString() + 'm';
+          ],
+          yAxes: [
+            {
+              type: "linear",
+              ticks: {
+                userCallback: function(tick) {
+                  return tick.toString() + "m";
+                }
+              },
+              scaleLabel: {
+                labelString: "Tempo",
+                display: true
               }
-            },
-            scaleLabel: {
-              labelString: 'Höhe',
-              display: true
             }
-          }]
+          ]
         }
       }
     });
   }
 }
-
-
